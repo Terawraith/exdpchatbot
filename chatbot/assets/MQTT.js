b547e8327f0c6c3125536239b2d3bf0c08c4fdb8
Terawraith <jaar@itu.dk>
2020-05-27 21:07:48 +0200

    Update MQTT.js

--- chatbot/assets/MQTT.js
+++ chatbot/assets/MQTT.js
@@ -1,9 +1,9 @@
 // SETUP MQTT ------------------------------------------
 
-const broker = "test.mosquitto.org";
-const port = 8081 ;
+const broker = "influx.itu.dk";
+const port = 9002;
 const secured = true;
-const topic="exdpchatbot";
+const topic="ituF2020/EXPD/exdpchatbot";
 const myID = "id" + parseInt(Math.random() * 100000, 10);
 
 
@@ -11,8 +11,8 @@ const myID = "id" + parseInt(Math.random() * 100000, 10);
 // CONNECT ----------------------------------------------
 
 let client = new Paho.MQTT.Client( broker, port, myID);
-console.log("let client: ");
-console.log(client);
+//console.log("let client: ");
+//console.log(client);
 
 	// function to trigger connection
 
@@ -41,28 +41,29 @@ function onConnect()
 	// call to send a message
 function sendMessage(msg) {
 	// document.cookie = "username=" + clientIdReceived;    
-	 console.log("sendMessage: " )
-	 console.log(msg);
+	// console.log("sendMessage: " )
+	// console.log(msg);
+	console.log("Message sent from: " + myID)
 	 let mObj = { deviceId: myID, content: msg };
 	 let mSend = new Paho.MQTT.Message(JSON.stringify(mObj));
 	 mSend.destinationName = topic;
-	 console.log("mSend: ");
-	 console.log(mSend);
+//	 console.log("mSend: ");
+//	 console.log(mSend);
 	 client.send(mSend);
 }
 
 	// called when a message arrives
-// called when a message arrives
+   	// called when a message arrives
 function receiveMessage(msg) {
-  console.log("msg recieved: ");
-  console.log(msg);
+//  console.log("msg recieved: ");
+//  console.log(msg);
     let mUnpack = JSON.parse(msg.payloadString);
-  console.log("mUnpack: ");
-  console.log(mUnpack);
+//  console.log("mUnpack: ");
+//  console.log(mUnpack);
     let receivedMessage = mUnpack.content;
     let clientIdReceived = mUnpack.deviceId;
-  console.log("clientRecieved: ");
-  console.log(clientIdReceived);
+//  console.log("clientRecieved: ");
+//  console.log(clientIdReceived);
     //let userCoords = [lat, lon];
   console.log("recievedMessage: ")
   console.log(receivedMessage);
